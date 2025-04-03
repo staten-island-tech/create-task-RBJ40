52f16905652b82599af5f7c9a00674485edebea4
RBJ40 <celinechan268@gmail.com>
2025-04-02 21:33:05 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,47 +3,55 @@ const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
 const playerChoices = [rock, paper, scissors];
 const computerChoices = ["Rock", "Paper", "Scissors"];
+let playerWins = 0;
+let computerWins = 0;
 
 playerChoices.forEach((option) => {
   option.addEventListener("click", function () {
-    const choiceNumber = Math.floor(Math.random() * 3);
-    const computerChoice = computerChoices[choiceNumber];
-    gameStart(this.innerText, computerChoice);
+    if (playerWins < 3 && computerWins < 3) {
+      const choiceNumber = Math.floor(Math.random() * 3);
+      const computerChoice = computerChoices[choiceNumber];
+      gameStart(this.innerText, computerChoice);
+    }
   });
 });
 
 function gameStart(player, computer) {
   const result = document.querySelector(".result");
-  let wins = 0;
-  while (wins < 3) {
-    if (player === computer) {
-      result.textContent = "Tie";
-    } else if (player == "Rock") {
-      if (computer == "Paper") {
-        result.textContent = "Computer won";
-      } else {
-        result.textContent = "Player won";
-        wins++;
-      }
-    } else if (player == "Paper") {
-      if (computer == "Scissors") {
-        result.textContent = "Computer won";
-      } else {
-        result.textContent = "Player won";
-        wins++;
-      }
-    } else if (player == "Scissors") {
-      if (computer == "Rock") {
-        result.textContent = "Computer won";
-      } else {
-        result.textContent = "Player won";
-        wins++;
-      }
-      if (computer.wins == 3) {
-        result.textContent = "Computer won three times";
-      } else if (player.wins == 3) {
-        result.textContent = "Player won three times";
-      }
+   if (player === computer) {
+    result.textContent = "Tie";
+  } else if (player === "Rock") {
+    if (computer === "Paper") {
+      computerWins++;
+      result.textContent = "Computer won this round";
+    } else {
+      playerWins++;
+      result.textContent = "Player won this round";
+    }
+  } else if (player === "Paper") {
+    if (computer === "Scissors") {
+      computerWins++;
+      result.textContent = "Computer won this round";
+    } else {
+      playerWins++;
+      result.textContent = "Player won this round";
+    }
+  } else if (player === "Scissors") {
+    if (computer === "Rock") {
+      computerWins++;
+      result.textContent = "Computer won this round";
+    } else {
+      playerWins++;
+      result.textContent = "Player won this round";
+    }
+  }
+  while (playerWins < 3 && computerWins < 3) {
+    if (computerWins === 3) {
+      result.textContent = "Computer won three times. Game over!";
+      break;
+    } else if (playerWins === 3) {
+      result.textContent = "Player won three times. Game over!";
+      break;
     }
   }
 }
